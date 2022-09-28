0866a472a64932a8182ccecfb2ceba4e17f9b9ea
AnnaGrace-Petty <annagracepetty@comcast.net>
2022-09-28 15:48:25 -0500

    second push

--- scripts/prescription_drug_project_ap.sql
+++ scripts/prescription_drug_project_ap.sql
@@ -20,10 +20,50 @@ GROUP BY specialty_description
 ORDER BY total_drug_claim DESC ;
 
 --QUESTION 2b
-SELECT specialty_description, total_claim_count
+SELECT specialty_description, SUM(total_claim_count) AS total_drug_claim
 FROM prescriber
 INNER JOIN prescription
 USING (npi)
-WHERE drug_name LIKE 'opioids'
+INNER JOIN drug
+USING (drug_name)
+WHERE opioid_drug_flag = 'Y'
 GROUP BY specialty_description, total_claim_count
-ORDER BY total_claim_count DESC ;
+ORDER BY total_drug_claim DESC
+LIMIT 1 ;
+
+--QUESTION 3a
+SELECT generic_name, sum (total_drug_cost) AS drug_cost
+FROM drug
+INNER JOIN prescription
+USING (drug_name)
+GROUP BY generic_name
+ORDER BY drug_cost
+LIMIT 1 ;
+
+--QUESTION 3b
+SELECT generic_name, ROUND(sum(total_drug_cost)/ sum(total_day_supply),2) AS cost_per_day
+FROM drug
+INNER JOIN prescription
+USING(drug_name)
+GROUP BY generic_name
+ORDER BY cost_per_day
+LIMIT 1 ;
+
+--QUESTION 4a
+SELECT drug_name,
+ CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+      WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+	  WHEN opioid_drug_flag = 'N' AND antibiotic_drug_flag = 'N' THEN 'neither' END AS drug_type
+FROM drug
+WHERE opioid_drug_flag IS NOT NULL
+ AND antibiotic_drug_flag IS NOT NULL ;
+ 
+--QUESTION 4b
+
+ 
+
+
+
+
+
+
